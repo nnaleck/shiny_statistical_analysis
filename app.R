d956f832df75a4e6a0de039cf39bc86fe3af4b36
nnaleck <ilyes_faicel.kamel@g.enp.edu.dz>
2021-01-26 16:38:22 +0100

    wip

--- app.R
+++ app.R
@@ -16,60 +16,143 @@ biomass[is.na(biomass)] <- 0
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
+    
     # Application title
     titlePanel("Biomass Data Univariate statiscial analysis"),
-
-    fluidRow(
-        column(6, selectInput("select", label = h3("Select feature"), 
-                              choices = cbind(names(biomass)), 
-                              selected = 1))
-    ),
     
-    fluidRow(
-        conditionalPanel(
-            condition="input.select != 'species' && input.select != 'fac26'",
-            fluidRow(
-                column(4, tableOutput("statsTableOut")),
-                column(8, plotOutput("boxplotOut"))
-            )
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("select", label = h3("Select feature"), 
+                        choices = names(biomass), 
+                        selected = 1)
         ),
-        column(12, plotOutput("batonsOut"))
-    ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+            tabsetPanel(
+                tabPanel("summary", tableOutput("statsTableOut")),
+                tabPanel("plots",
+                         fluidRow(
+                             column(6, 
+                                    # Zone d'affichage de l'histogramme
+                                    plotOutput(outputId = "effectifsHist")),
+                             column(6, 
+                                    # Zone d'affichage d'un summary
+                                    plotOutput(outputId = "frequenceHist"))
+                         ),
+                         fluidRow(
+                             column(6, 
+                                    # Zone d'affichage de l'histogramme
+                                    plotOutput(outputId = "effectifsCumCurve")),
+                             column(6, 
+                                    # Zone d'affichage d'un summary
+                                    plotOutput(outputId = "boxplot"))
+                         ))
+                         
+            )
+        )
+    )
+    
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    tableStats <- reactive({
-        if (!is.numeric(biomass[, input$select])) return(NULL)
-        
-        names.tmp <- c('Max', 'Min', 'Moyenne', '1e quartile', 'Médiane', '3e quartile', 'Variance', 'Écart-type')
-        summary.tmp <- c(
-            max(biomass[, input$select]), min(biomass[, input$select]), mean(biomass[, input$select]),
-            quantile(biomass[, input$select])[2], median(biomass[, input$select]), quantile(biomass[, input$select])[4],
-            var(biomass[, input$select]), sqrt(var(biomass[, input$select]))
-            )
+    # Colonnes du tableau statistique[qualitative]
+    tabStatsQual <- reactive({
+        # Calculer les effectifs et les effectifs cumules
+        table.tmp <- as.data.frame(table(biomass[, input$select]))
+        table.tmp <- cbind(table.tmp, cumsum(table.tmp[[2]]))
+        # Calculer les fréquences et les fréquences cumulés
+        table.tmp <- cbind(table.tmp, 
+                           table.tmp[[2]]/nrow(biomass)*100,
+                           table.tmp[[3]]/nrow(biomass)*100)
+        # Ajouter des noms de colonnes
+        colnames(table.tmp) <- c(input$select, "Effectifs", "Effectifs Cum.",
+                                 "Fréquences", "Fréquences Cum.")
+        # Renvoyer le tableau statistique
+        table.tmp
+    })
+    # Colonnes du tableau statistique[quantitative]
+    tabStatsQuant <- reactive({
+        names.tmp <- c('Max', 'Min', 'Moyenne')
+        summary.tmp <- c(max(biomass[, input$select]), min(biomass[, input$select]), mean(biomass[, input$select]))
         summary.tmp <- cbind.data.frame(names.tmp, summary.tmp)
         
         colnames(summary.tmp) <- c('Statistique', 'Valeur')
         
         summary.tmp
     });
+    output$statsTableOut <- renderTable({ 
+        if (is.numeric(biomass[, input$select])){
+            tabStatsQuant()
+        }else{
+            tabStatsQual() 
+        }
+        })
     
- 
-    output$statsTableOut <- renderTable({ tableStats() })
-    output$boxplotOut <- renderPlot({
-        if (!is.numeric(biomass[, input$select])) return(NULL)
-        
-        boxplot(biomass[, input$select], col = grey(0.8), main="Boîte à moustaches", ylab=input$select)
+    # Histogramme des effectifs
+    output$effectifsHist <- renderPlot({
+        if (is.numeric(biomass[, input$select])){
+            hist( biomass[, input$select], freq = TRUE, col = "blue",
+                  main = paste("Histogramme de", input$select, sep=" "),
+                  xlab = paste("Indice de", input$select, sep=" "), ylab = "Effectifs")
+        }else{
+            # Calcul des effectifs
+            effectifs <- table(biomass[, input$select])
+            # Diagramme en colonnes
+            barplot(effectifs, ylab="Effectifs",
+                        main = paste("diagramme en baton de ", input$select, sep=""))
+                
+        }
+         
     })
     
-    output$batonsOut <- renderPlot({
-        plot(table(biomass[, input$select]), col ="purple4", xlab =input$select, ylab ="Effectifs", 
-             main ="Distribution des effectifs pour la colonne")
+    # Boxplot
+    output$boxplot <- renderPlot({
+        if (is.numeric(biomass[, input$select])){
+            boxplot(biomass[, input$select], main=paste("Boxplot de", input$select, sep=" "))
+        }else{
+            return(NULL)
+        }
+    })
+    
+    #Courbe cumulative
+    output$effectifsCumCurve <- renderPlot({
+        if (is.numeric(biomass[, input$select])){
+            #Recuperation des donnees a partir de l'histogramme
+            tmp.hist <- hist( biomass[, input$select], plot = FALSE,
+                            right = FALSE)
+        
+            plot(x = tmp.hist$breaks[-1], y = cumsum(tmp.hist$counts),
+                xlab = input$select,
+                ylab = "Effectifs cumules",
+                main = paste("Courbe cumulative de ", input$select, sep=""),
+                type = "o", col = "blue", lwd = 2)
+        }else{
+            return(NULL)
+        }
+        
+    })
+    
+    #Histogramme(frequencies)
+    output$frequenceHist <- renderPlot({
+        if (is.numeric(biomass[, input$select])){
+             hist( biomass[, input$select], freq = FALSE,
+                main = paste("Histogramme de ", input$select, sep=""), col = "green",
+                xlab = input$select, ylab = "Densite de frequences", 
+                right = FALSE,)
+        }else{
+            # Calcul des effectifs
+            effectifs <- table(biomass[, input$select])
+            #Diagramme en secteur
+            pie(effectifs, 
+                main =paste("diagramme en secteurs de ", input$select, sep=""))
+        }
     })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+
